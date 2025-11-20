298b269a0af88514b194aa2b0adebd253992c214
Ouattara Dongui <ouattaradongui123@gmail.com>
2025-11-20 17:22:11 +0100

    First commit

---
+++ app_generateur/src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { routeTree } from './routeTree.gen'
+import './index.css'
+
+const router = createRouter({ routeTree })
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>,
+)
